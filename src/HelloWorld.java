808b2c19e3984f9fb37ce352f24cc1597d7a24ca
Marcus Luk <marcus.luk@oocl.com>
2020-11-13 11:18:32 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+import model.Student;
+
+public class HelloWorld {
+    public static void main(String argc[]){
+        Student alice = new Student("alice",18,"female", "123456");
+        System.out.println(alice);
+    }
+}
